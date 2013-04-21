50419b86251a9935056c8d86962bef7a2f813775
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-21 11:41:32 +0200

    Remove specific packages to install.

--- init.el
+++ init.el
@@ -18,24 +18,3 @@
   (dolist (p packs)
     (when (not (package-installed-p p))
       (package-install p))))
-
-(defvar my-packages '(switch-window
-                      ;; compile on the fly
-                      flymake
-                      flymake-shell
-                      ;; some playtime with graphviz
-                      graphviz-dot-mode
-                      fold-dwim
-                      htmlize
-                      ;; to help in editing textare in chrome
-                      edit-server
-                      ;; to display puppet file
-                      puppet-mode
-                      ;; ghci-completion
-                      ;; to make some awesome stuff on multiple line in one time
-                      multiple-cursors
-                      ;; move
-                      move-text)
-  "A list of packages to ensure are installed at launch.")
-
-(install-packs my-packages)
