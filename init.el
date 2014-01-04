1ff9d6cc1246386762989a303f3573a744af40c2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-04 03:18:23 +0100

    Give the means to temporarily override the 'package-archives to install package from other package repository

--- init.el
+++ init.el
@@ -4,10 +4,8 @@
 (setq url-proxy-services '(("no_proxy" . "work\\.com")))
 
 (when (< emacs-major-version 24)
-      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
-;; (add-to-list 'package-archives
-;;              '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 ;; (add-to-list 'package-archives
@@ -18,12 +16,22 @@
 
 (package-initialize)
 
-(defun install-pack (p)
-  "A utility function to help in installing emacs package."
+(defvar *INSTALL-PACKAGES-PACK-BACKUP* nil)
+
+(defun install-packages-pack/--install-package-archives (pack-archives)
+  (setq *INSTALL-PACKAGES-PACK-BACKUP* package-archives)
+  (setq package-archives pack-archives)
+  (package-refresh-contents))
+
+(defun install-packages-pack/--reset-package-archives ()
+  (setq package-archives *INSTALL-PACKAGES-PACK-BACKUP*)
+  (package-refresh-contents))
+
+(defun install-pack (p) "A utility function to help in installing emacs package."
   (unless (package-installed-p p) (package-install p)))
 
-(defun install-packs (packs)
-  "A utility function to help in installing emacs packages."
-  (unless package-archive-contents
-          (package-refresh-contents))
-  (dolist (p packs) (install-pack p)))
+(defun install-packs (packs &optional pack-archives) "A utility function to help in installing emacs packages."
+  (when pack-archives (install-packages-pack/--install-package-archives pack-archives))
+  (unless package-archive-contents (package-refresh-contents))
+  (dolist (p packs) (install-pack p))
+  (when pack-archives (install-packages-pack/--reset-package-archives)))
