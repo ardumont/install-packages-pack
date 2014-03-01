f6091a2ddd633ca95d75334fd366132ac3ee5db8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 16:10:13 +0100

    Simplify packages installation

--- init.el
+++ init.el
@@ -9,16 +9,14 @@
 ;; deactivate any proxy (got some trouble with authentication)
 (defvar url-proxy-services '(("no_proxy" . "work\\.com")))
 
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.milkbox.net/packages/")
+;;                         ("tromey"    . "http://tromey.com/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
-;; marmalade as default (not melpa as some deps are unstable)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-;; when wanting to install from only elpa or marmalade
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")  ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
-(package-refresh-contents)
+(unless package-archive-contents (package-refresh-contents))
 
 (defun install-pack (pack) "A utility function to help in installing emacs package."
   (unless (package-installed-p pack)
@@ -27,26 +25,10 @@
 (install-pack 'dash)
 (require 'dash)
 
-(defvar *INSTALL-PACKAGES-PACK-BACKUP* nil)
-
-(defun install-packages-pack/--install-temporary-package-archives (pack-archives) "When you need to install some dependencies from another repository you do not want as default (for example melpa)"
-  (setq *INSTALL-PACKAGES-PACK-BACKUP* package-archives)
-  (setq package-archives pack-archives)
-  (package-refresh-contents))
-
-(defun install-packages-pack/--reset-temporary-package-archives () "Reset to normal repo archives"
-  (setq package-archives *INSTALL-PACKAGES-PACK-BACKUP*)
-  (package-refresh-contents))
-
-(defun install-packages-pack/--filter-packs-to-install (pack-archives) "Is there any pack from the list already installed?"
-  (-filter (lambda (pack) (not (package-installed-p pack))) pack-archives))
-
-(defun install-packs (packs &optional pack-archives) "A utility function to help in installing emacs packages."
-  (-when-let (new-packs (install-packages-pack/--filter-packs-to-install packs))
-             (let ((install-temporary-packages-p (and new-packs pack-archives)))
-               (when install-temporary-packages-p (install-packages-pack/--install-temporary-package-archives pack-archives))
-               (unless package-archive-contents (package-refresh-contents))
-               (dolist (p new-packs) (install-pack p))
-               (when install-temporary-packages-p (install-packages-pack/--reset-temporary-package-archives)))))
+(defun install-packs (packs)
+  "A utility function to help installing emacs packages."
+  (->> packs
+       (--filter (not (package-installed-p it)))
+       (mapc 'install-pack)))
 
 ;;; install-packages-pack.el ends here
