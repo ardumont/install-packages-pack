c15c5b6ced549782d7e313138f9fe2f349df656d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 18:12:01 +0100

    Rewrite the packages repositoryies installation. Now computing the difference and if there is some, a package refresh takes place

--- init.el
+++ init.el
@@ -5,25 +5,34 @@
 ;;; Code:
 
 (require 'package)
+(require 'dash)
 
 ;; deactivate any proxy (got some trouble with authentication)
 (defvar url-proxy-services '(("no_proxy" . "work\\.com")))
 
-(setq package-archives '(;; ("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")
-                         ;; ("tromey"    . "http://tromey.com/elpa/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(defun --filter-repositories (repos archives) "Given a list of couple repository name, repository url, return the list of not associated entries."
+  (--filter (not (assoc-default (car it) archives)) repos))
 
-(package-initialize)
-(unless package-archive-contents (package-refresh-contents))
+(defun update-repositories-archives! (repos)
+  (package-initialize)
+  "Given a list of repositories, update the package-archives if only new repositories are present"
+  (-when-let (repos-to-add (--filter-repositories repos package-archives))
+    (message "Repos to add: %s" repos-to-add)
+    ;; we need to add the list of repos to the archives
+    (--map (add-to-list 'package-archives it) repos-to-add)
+    (message "new archive: %s" package-archives)
+    ;; we need to refresh the packages index
+    (package-refresh-contents)))
+
+(update-repositories-archives! '(;; ("org"      . "http://orgmode.org/elpa/")
+                                 ("gnu"       . "http://elpa.gnu.org/packages/")
+                                 ("melpa"     . "http://melpa.milkbox.net/packages/")
+                                 ;; ("tromey"    . "http://tromey.com/elpa/")
+                                 ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (defun install-pack (pack) "A utility function to help in installing emacs package."
   (unless (package-installed-p pack)
           (package-install pack)))
-
-(install-pack 'dash)
-(require 'dash)
 
 (defun install-packs (packs)
   "A utility function to help installing emacs packages."
