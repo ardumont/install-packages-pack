44c9cd5360801177ee3f026de343e65ada688f5f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-27 11:35:06 +0100

    Use 'names package which enhance emacs-lisp with namespaces abilities

--- install-packages-pack.el
+++ install-packages-pack.el
@@ -22,22 +22,26 @@
 (require 'package)
 (mapc 'package-install '(paradox dash names))
 
-(require 'paradox)
-(defvar install-packages-pack/paradox-setup-file (expand-file-name "~/.emacs.d/.paradox/setup.el")) ;; token installed manually the first time M-x paradox-list-packages is called
+(require 'names) ;; to add namespaces abilities to emacs-lisp
 
-(when (file-exists-p (expand-file-name install-packages-pack/paradox-setup-file))
-  (load-file install-packages-pack/paradox-setup-file))
+(define-namespace install-packages-pack/
+
+(require 'paradox)
+(defvar paradox-setup-file (expand-file-name "~/.emacs.d/.paradox/setup.el")) ;; token installed manually the first time M-x paradox-list-packages is called
+
+(when (file-exists-p (expand-file-name paradox-setup-file))
+  (load-file paradox-setup-file))
 
 (require 'dash)
 
-(defun install-packages-pack/--filter-repositories (repos archives)
+(defun --filter-repositories (repos archives)
   "Given a list REPOS of couple (repository name, repository url) and a list of ARCHIVES, return the list of not associated entries."
   (--filter (not (assoc-default (car it) archives)) repos))
 
-(defun install-packages-pack/update-repositories-archives! (repos)
+(defun update-repositories-archives! (repos)
   "Given a list of repositories REPOS, update the package-archives if only new repositories are present."
   (package-initialize)
-  (-when-let (repos-to-add (install-packages-pack/--filter-repositories repos package-archives))
+  (-when-let (repos-to-add (--filter-repositories repos package-archives))
     (message "Repos to add: %s" repos-to-add)
     ;; we need to add the list of repos to the archives
     (mapc (lambda (repo) (add-to-list 'package-archives repo 'append)) repos-to-add)
@@ -45,24 +49,26 @@
     ;; we need to refresh the packages index
     (package-refresh-contents)))
 
-(install-packages-pack/update-repositories-archives! '(("org"      . "http://orgmode.org/elpa/")
-                                                      ;; ("gnu"       . "http://elpa.gnu.org/packages/")
-                                                      ("melpa"     . "http://melpa.milkbox.net/packages/")
-                                                      ;; ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                                                      ;; ("tromey"    . "http://tromey.com/elpa/")
-                                                      ("marmalade" . "http://marmalade-repo.org/packages/")
-                                                      ))
+(update-repositories-archives! '(("org"      . "http://orgmode.org/elpa/")
+                                 ;; ("gnu"       . "http://elpa.gnu.org/packages/")
+                                 ("melpa"     . "http://melpa.milkbox.net/packages/")
+                                 ;; ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+                                 ;; ("tromey"    . "http://tromey.com/elpa/")
+                                 ("marmalade" . "http://marmalade-repo.org/packages/")
+                                 ))
 
-(defun install-packages-pack/install-pack (pack)
+(defun install-pack (pack)
   "A utility function to help in installing an Emacs package PACK."
   (unless (package-installed-p pack)
     (package-install pack)))
 
-(defun install-packages-pack/install-packs (packs)
+:autoload
+(defun install-packs (packs)
   "A utility function to help installing a list PACKS of Emacs packages."
   (->> packs
     (--filter (not (package-installed-p it)))
-    (mapc 'install-packages-pack/install-pack)))
+    (mapc 'install-pack)))
+)
 
 (provide 'install-packages-pack)
 ;;; install-packages-pack.el ends here
