c0ac936d58878da07d2cac16007fa4f25e39bcec
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-05-26 23:01:24 +0200

    Simplify the packs' setup

--- install-packages-pack.el
+++ install-packages-pack.el
@@ -21,47 +21,31 @@
 
 (require 'package)
 
-(unless package--initialized ;; the first time we hit this (typically in emacs-live)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (package-refresh-contents)
-  (package-install 'dash))
+(mapc (lambda (repo) (add-to-list 'package-archives repo))
+      '(("org"      . "http://orgmode.org/elpa/")
+	;; ("gnu"       . "http://elpa.gnu.org/packages/")
+	("melpa"     . "http://melpa.milkbox.net/packages/")
+	;; ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+	;; ("tromey"    . "http://tromey.com/elpa/")
+	("marmalade" . "https://marmalade-repo.org/packages/")))
 
-(require 'dash)
+(eval-when-compile (unless package--initialized (package-initialize)))
 
-(defun install-packages-pack/--filter-repositories (repos archives)
-  "Given a list REPOS of couple (repository name, repository url) and a list of ARCHIVES, return the list of not associated entries."
-  (--filter (not (assoc-default (car it) archives)) repos))
-
-(defun install-packages-pack/update-repositories-archives! (repos)
-  "Given a list of repositories REPOS, update the package-archives if only new repositories are present."
-  (-when-let (repos-to-add (install-packages-pack/--filter-repositories repos package-archives))
-    (message "Repos to add: %s" repos-to-add)
-    ;; we need to add the list of repos to the archives
-    (mapc (lambda (repo) (add-to-list 'package-archives repo 'append)) repos-to-add)
-    (message "new archive: %s" package-archives)
-    (package-initialize)
-    (package-refresh-contents)))    ;; we need to refresh the packages index
-
+;;;###autoload
 (defun install-packages-pack/install-pack (pack)
   "A utility function to help in installing an Emacs package PACK."
-  (unless (package-installed-p pack)
-    (package-install pack)))
+  (let* ((pack-archives (if (null package-archive-contents)
+			    (progn (package-refresh-contents)
+				   package-archive-contents)
+			  package-archive-contents))
+	 (pack-available-p (assoc pack pack-archives)))
+    (when (and pack-available-p (not (package-installed-p pack)))
+      (package-install pack))))
 
 ;;;###autoload
 (defun install-packages-pack/install-packs (packs)
   "A utility function to help installing a list PACKS of Emacs packages."
-  (->> packs
-    (--filter (not (package-installed-p it)))
-    (mapc #'install-packages-pack/install-pack)))
-
-(install-packages-pack/update-repositories-archives! '(("org"      . "http://orgmode.org/elpa/")
-                                                       ;; ("gnu"       . "http://elpa.gnu.org/packages/")
-                                                       ("melpa"     . "http://melpa.milkbox.net/packages/")
-                                                       ;; ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                                                       ;; ("tromey"    . "http://tromey.com/elpa/")
-                                                       ("marmalade" . "https://marmalade-repo.org/packages/")
-                                                       ))
+  (mapc #'install-packages-pack/install-pack packs))
 
 (provide 'install-packages-pack)
 ;;; install-packages-pack.el ends here
